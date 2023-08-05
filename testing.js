c763acc51c56e7aa3debf07248f2881e9aa5cd21
narendramamtora <137789140+narendramamtora@users.noreply.github.com>
2023-08-06 05:11:29 +0530

    testing1

---
+++ testing.js
@@ -0,0 +1,11 @@
+const person={
+    name: "max",
+    age:29,
+    get(){
+        console.log('Hi this is '+this.name);
+    }
+};
+const hobbies=['storts', 'cooking',2]
+for(let hoy of hobbies){
+    console.log(hoy);
+}
