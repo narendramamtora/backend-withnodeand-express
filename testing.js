9507b70b5b6bb92efc4cd77c3897fe729c14504c
narendramamtora <137789140+narendramamtora@users.noreply.github.com>
2023-08-06 05:14:04 +0530

    testing2

--- testing.js
+++ testing.js
@@ -9,3 +9,8 @@ const hobbies=['storts', 'cooking',2]
 for(let hoy of hobbies){
     console.log(hoy);
 }
+let re=hobbies.map(news=>'hello '+news) 
+console.log(re)
+//adding the new item in the array using push 
+hobbies.push(12);
+console.log(hobbies);
