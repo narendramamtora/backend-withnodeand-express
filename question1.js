2c9156e2cc41095eafe324992fe0238c8fa2fd24
narendramamtora <137789140+narendramamtora@users.noreply.github.com>
2023-08-07 17:27:00 +0530

    welcome message question1.js

--- question1.js
+++ question1.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type','text/html');
   res.write('<html>');
   res.write('<head><title>My first</title></head>');
-  res.write('<body><h1>first js server</h1></body>');
+  res.write('<body><h1>Welcome to my Node Js Project</h1></body>');
   res.write('</html>')
   res.end();
 });
