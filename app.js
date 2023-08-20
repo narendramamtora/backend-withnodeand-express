44063b68538cbb790085124dda01a1b166e7f927
narendramamtora <137789140+narendramamtora@users.noreply.github.com>
2023-08-20 23:22:38 +0530

    creating and adding items to cart

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorcontroller=require('./controllers/error')
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorcontroller.error404);
+app.use(errorController.get404);
 
 app.listen(3000);
